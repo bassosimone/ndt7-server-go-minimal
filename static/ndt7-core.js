6f6d54ed81d5c40f2c7eedb5280971b1fe2dd554
Simone Basso <bassosimone@gmail.com>
2020-03-13 00:12:09 +0100

    ndt7-core.js: make sure user has given us permission

--- static/ndt7-core.js
+++ static/ndt7-core.js
@@ -2,19 +2,39 @@
 // ndt7 is a simple ndt7 client.
 const ndt7 = (function() {
   return {
-    // run runs the specified test with the specified base URL and calls
-    // handler's callbacks to notify the caller of ndt7 events.
-    run: function(baseURL, testName, handler) {
-      if (handler !== undefined && handler.onstarting !== undefined) {
-        handler.onstarting({Origin: "client", Test: testName})
+    // run runs the specified test with the specified config. The config
+    // object structure is as follows:
+    //
+    //     {
+    //       baseURL: "",
+    //       oncomplete: function() {},
+    //       onmeasurement: function (measurement) {},
+    //       onstarting: function() {},
+    //       testName: "",
+    //       userAcceptedDataPolicy: true
+    //     }
+    //
+    // where baseURL is the mandatory URL; oncomplete, omeasurement, and
+    // onstarting are optional handlers; testName is the mandatory test name
+    // and must be one of "download", "upload"; userAcceptedDataPolicy is
+    // a boolean that MUST be true for the measurement to start.
+    //
+    // The measurement is described by the ndt7 specification. See
+    // https://github.com/m-lab/ndt-server/blob/master/spec/ndt7-protocol.md.
+    run: function(config) {
+      if (config === undefined || config.userAcceptedDataPolicy !== true) {
+        throw "fatal: user must accept data policy first"
+      }
+      if (config.onstarting !== undefined) {
+        config.onstarting()
       }
       let done = false
-      let worker = new Worker("ndt7-" + testName + ".js")
+      let worker = new Worker("ndt7-" + config.testName + ".js")
       function finish() {
         if (!done) {
           done = true
-          if (handler !== undefined && handler.oncomplete !== undefined) {
-            handler.oncomplete({Origin: "client", Test: testName})
+          if (config.oncomplete !== undefined) {
+            config.oncomplete()
           }
         }
       }
@@ -23,8 +43,8 @@ const ndt7 = (function() {
           finish()
           return
         }
-        if (handler !== undefined && handler.onmeasurement !== undefined) {
-          handler.onmeasurement(ev.data)
+        if (config.onmeasurement !== undefined) {
+          config.onmeasurement(ev.data)
         }
       }
       // Kill the worker after the timeout. This force the browser to
@@ -34,7 +54,7 @@ const ndt7 = (function() {
         finish()
       }, 10000)
       worker.postMessage({
-        href: baseURL,
+        href: config.baseURL,
       })
     }
   }
