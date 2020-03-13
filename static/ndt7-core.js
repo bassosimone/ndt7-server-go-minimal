f9b8b644c03070cc16638bf7ea8264fff5aab6c3
Simone Basso <bassosimone@gmail.com>
2020-03-13 02:07:04 +0100

    Start re-adding support for mlab-ns

--- static/ndt7-core.js
+++ static/ndt7-core.js
@@ -1,8 +1,58 @@
 /* jshint esversion: 6, asi: true */
 // ndt7 is a simple ndt7 client.
 const ndt7 = (function() {
+  function startWithBaseURL(config) {
+    if (config === undefined || config.userAcceptedDataPolicy !== true) {
+      throw "fatal: user must accept data policy first"
+    }
+    if (config.onstarting !== undefined) {
+      config.onstarting()
+    }
+    let done = false
+    let worker = new Worker("ndt7-" + config.testName + ".js")
+    function finish() {
+      if (!done) {
+        done = true
+        if (config.oncomplete !== undefined) {
+          config.oncomplete()
+        }
+      }
+    }
+    worker.onmessage = function (ev) {
+      if (ev.data === undefined) {
+        finish()
+        return
+      }
+      if (config.onmeasurement !== undefined) {
+        config.onmeasurement(ev.data)
+      }
+    }
+    // Kill the worker after the timeout. This forces the browser to
+    // close the WebSockets and prevent too-long tests.
+    setTimeout(function () {
+      worker.terminate()
+      finish()
+    }, 10000)
+    worker.postMessage({
+      href: config.baseURL,
+    })
+  }
+
+  function locate(config) {
+    if (config.url === undefined || config.url === "") {
+      config.url = "https://locate.measurementlab.net/ndt7"
+    }
+    fetch(config.url)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (doc) {
+        config.callback(doc.fqdn)
+      })
+  }
+
   return {
-    // run runs the specified test with the specified config. The config
+    // start starts the specified test with the specified config. The config
     // object structure is as follows:
     //
     //     {
@@ -14,47 +64,35 @@ const ndt7 = (function() {
     //       userAcceptedDataPolicy: true
     //     }
     //
-    // where baseURL is the mandatory URL; oncomplete, omeasurement, and
-    // onstarting are optional handlers; testName is the mandatory test name
-    // and must be one of "download", "upload"; userAcceptedDataPolicy is
-    // a boolean that MUST be true for the measurement to start.
+    // where
     //
-    // The measurement is described by the ndt7 specification. See
+    // - `baseURL` (`string`) is the optional http/https URL of the server;
+    //
+    // - `oncomplete` (`function()`) is the optional callback called when done;
+    //
+    // - `onmeasurement` (`function(measurement)`) is the optional callback
+    //   called when a new measurement object is emitted (see below);
+    //
+    // - `onstarting` is like `oncomplete` but called at startup;
+    //
+    // - `testName` (`string`) is one of "download", "upload";
+    //
+    // - `userAcceptedDataPolicy` MUST be present and set to true.
+    //
+    // If `baseURL` is missing, we will locate a nearby server using mlab-ns.
+    //
+    // The measurement object is described by the ndt7 specification. See
     // https://github.com/m-lab/ndt-server/blob/master/spec/ndt7-protocol.md.
-    run: function(config) {
-      if (config === undefined || config.userAcceptedDataPolicy !== true) {
-        throw "fatal: user must accept data policy first"
+    start: function start(config) {
+      if (config.baseURL !== undefined && config.baseURL !== "") {
+        startWithBaseURL(config)
+        return
       }
-      if (config.onstarting !== undefined) {
-        config.onstarting()
-      }
-      let done = false
-      let worker = new Worker("ndt7-" + config.testName + ".js")
-      function finish() {
-        if (!done) {
-          done = true
-          if (config.oncomplete !== undefined) {
-            config.oncomplete()
-          }
-        }
-      }
-      worker.onmessage = function (ev) {
-        if (ev.data === undefined) {
-          finish()
-          return
-        }
-        if (config.onmeasurement !== undefined) {
-          config.onmeasurement(ev.data)
-        }
-      }
-      // Kill the worker after the timeout. This force the browser to
-      // close the WebSockets and prevent too-long tests.
-      setTimeout(function () {
-        worker.terminate()
-        finish()
-      }, 10000)
-      worker.postMessage({
-        href: config.baseURL,
+      locate({
+        callback: function (fqdn) {
+          config.baseURL = fqdn
+          startWithBaseURL(config)
+        },
       })
     }
   }
